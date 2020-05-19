186e19caa1230f89f6d7218b37ee5895666282c3
dhsazlan <43531860+azlansu@users.noreply.github.com>
2020-05-19 17:41:07 +0800

    Add files via upload

---
+++ inspect.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+function msg()
+{
+// alert("this is the alert");	
+var ddd = document.getElementById("tdata").innerHTML ;
+
+if (ddd=="" || ddd=="hahaha") {
+document.getElementById("tdata").innerHTML = "just simply data transfer";
+}
+else
+{
+	document.getElementById("tdata").innerHTML = "hahaha";
+}
+
+}
