98c12cdb0c59c560526a5baa3cfe74202d705a03
dhsazlan <43531860+azlansu@users.noreply.github.com>
2020-05-29 17:07:22 +0800

    Update hmenu.js

--- jsfile/hmenu.js
+++ jsfile/hmenu.js
@@ -1,58 +1,10 @@
-#menu_wrapper {
-width: 900px;
-height: 40px;
-background-color: #ddd;
-margin: 0 auto;
-margin-top: 50px;
+$(document).ready(function(){ 
 
-}
+    $("#menu ul li").hover (function() {
 
-* {
-    margin: 0px;
-    padding: 0px;
-    font-family: Helvetica;
-    font-size: 14px;
-}
+        $(this).find("ul") .slideToggle(400);
+
+    })
 
 
-#menu ul {
-list-style: none;
-background-color: blanchedalmond;
-position: relative;
-
-}
-
-#menu ul li {
-    display: inline-block;
-    width: 150px;
-    height: 40px;
-    text-align: center;
-    line-height: 40px;
-
-}
-
-#menu ul li a:link{
-    display: block;
-    background-color: chartreuse;
-    color: coral;
-    text-decoration: none;
-}
-
-#menu ul li a:hover{
-    background-color: cornflowerblue;
-    color: darkblue;
-}
-
-#menu ul ul {
-    position: absolute;
-    display: none;
-    color: darkred;
-    
-    }
-    
-    #menu ul ul li {
-        width: 170px;
-        display: block;
-    }
-
-
+})
