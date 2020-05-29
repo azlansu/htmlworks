c767ad8b159c1eebf1a1a0472f84718992f6f951
dhsazlan <43531860+azlansu@users.noreply.github.com>
2020-05-29 17:02:51 +0800

    Create hmenu.css

---
+++ cssfile/hmenu.css
@@ -0,0 +1,58 @@
+#menu_wrapper {
+width: 900px;
+height: 40px;
+background-color: #ddd;
+margin: 0 auto;
+margin-top: 50px;
+
+}
+
+* {
+    margin: 0px;
+    padding: 0px;
+    font-family: Helvetica;
+    font-size: 14px;
+}
+
+
+#menu ul {
+list-style: none;
+background-color: blanchedalmond;
+position: relative;
+
+}
+
+#menu ul li {
+    display: inline-block;
+    width: 150px;
+    height: 40px;
+    text-align: center;
+    line-height: 40px;
+
+}
+
+#menu ul li a:link{
+    display: block;
+    background-color: chartreuse;
+    color: coral;
+    text-decoration: none;
+}
+
+#menu ul li a:hover{
+    background-color: cornflowerblue;
+    color: darkblue;
+}
+
+#menu ul ul {
+    position: absolute;
+    display: none;
+    color: darkred;
+    
+    }
+    
+    #menu ul ul li {
+        width: 170px;
+        display: block;
+    }
+
+
